c661bc266a5ea9c9dd3954da0bff3eef463d9fa4
liaafk <kusumastutiaurelia@gmail.com>
2022-07-01 09:41:37 +0200

    preprocess geolife

---
+++ config.py
@@ -0,0 +1,5 @@
+RAW = "Geolife Trajectories 1.3/Data"
+PREPROCESSED = "geolife_dpstar_preprocessed"
+EPSILON = [0.5, 1, 1.5, 2]
+ATTACK = False
+RESULTS = "geolife_dpstar_results"
---
+++ dpstar_with_geolife.py
@@ -0,0 +1,10 @@
+import os
+from pathlib import Path
+import csv
+from datetime import datetime, timedelta
+import numpy as np
+import pandas as pd
+import requests
+
+import config
+
